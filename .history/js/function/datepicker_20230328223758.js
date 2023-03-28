d15a8bf11bddac2248ec565c2cce17016696c213
Danny <nqd230899@gmail.com>
2023-03-28 22:39:13 +0700

    Update

---
+++ .history/js/function/datepicker_20230328223758.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#datetimes').daterangepicker({
+    timePicker: true,
+    // singleDatePicker: true,
+    startDate: moment().startOf("hour"),
+    maxDate: moment().startOf("hour" + "minute" + "second"),
+    locale: {
+      format: "MM/DD/YYYY hh:mm:ss A",
+    },
+  });
+});
---
+++ .history/js/function/datepicker_20230328223836.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('#datetimes').daterangepicker({
+    timePicker: true,
+    // singleDatePicker: true,
+    startDate: moment().startOf("hour"),
+    maxDate: moment().startOf("hour" + "minute"),
+    locale: {
+      format: "MM/DD/YYYY hh:mm A",
+    },
+  });
+});
